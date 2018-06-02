31482ce5f55404b4a303683b188558d858ad632d
zhuo <hczyzlm@126.com>
2018-06-02 22:36:24 +0800

    创建工程

---
+++ RunTimeExercise/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RunTimeExercise
+//
+//  Created by zhuo on 2018/6/2.
+//  Copyright © 2018年 zhuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
